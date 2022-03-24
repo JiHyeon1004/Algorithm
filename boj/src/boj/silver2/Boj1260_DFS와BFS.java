89cf51cd7ff872a46107ccaa458e9da40f89604b
JiHyeon Jeong <100845833+JiHyeon1004@users.noreply.github.com>
2022-03-24 22:48:03 +0900

    Updated on Mar 24 22:47

--- boj/src/boj/silver2/Boj1260_DFS와BFS.java
+++ boj/src/boj/silver2/Boj1260_DFS와BFS.java
@@ -13,53 +13,73 @@ public class Boj1260_DFS와BFS {
 	static int[][] tree;
 
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int V = Integer.parseInt(st.nextToken());
-		
-		tree = new int[N+1][N+1];
-		did = new boolean[N+1];
-		
-		for(int i = 0; i < M; i++) {
+
+		tree = new int[N + 1][N + 1];
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
+
 			tree[a][b] = 1;
 			tree[b][a] = 1;
 		}
-		
+
+		did = new boolean[N + 1];
 		DFS(V);
-		System.out.println(sb);
-		
+		sb.append("\n");
+		did = new boolean[N + 1];
+		BFS(V);
+		System.out.println(sb.toString());
+
 	}
-	
-	//DFS(깊이우선. 재귀함수)
-	
+
+	// DFS(깊이우선. 재귀함수)
+
 	public static void DFS(int node) {
-		
+
 		did[node] = true;
-		sb.append(node);
-		if(node == tree.length-1) {
-			return;	
-		}
-		for(int i = 0; i < tree.length; i++) {
-			if(did[node] == false && tree[node][i] == 1) {
+		sb.append(node).append(" ");
+//		if (node == tree.length - 1) {
+//			return;
+//		}
+		for (int i = 0; i < tree.length; i++) {
+			if (did[i] == false && tree[node][i] == 1) {
 				DFS(i);
 			}
 		}
-		
-		
+
 	}
-	
+
+	// 너비우선, queue
 	public static void BFS(int node) {
 		Queue<Integer> bfs = new LinkedList<Integer>();
-		
+
+		bfs.add(node);
+		did[node] = true;
+
+		sb.append(node).append(" ");
+
+		while (!bfs.isEmpty()) {
+			int tempNode = bfs.poll();
+
+			for (int i = 0; i < tree.length; i++) {
+				if (did[i] == false && tree[tempNode][i] == 1) {
+					bfs.add(i);
+					did[i] = true;
+					sb.append(i).append(" ");
+
+				}
+			}
+		}
+
 	}
-	
-	
+
 }
