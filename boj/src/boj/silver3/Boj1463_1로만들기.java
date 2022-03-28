6a15823c8ddb4ab4cf9cae12824463f87f78d717
JiHyeon Jeong <100845833+JiHyeon1004@users.noreply.github.com>
2022-03-28 21:01:35 +0900

    Updated on Mar 28 21:01

---
+++ boj/src/boj/silver3/Boj1463_1로만들기.java
@@ -0,0 +1,34 @@
+package boj.silver3;
+
+import java.util.Scanner;
+
+public class Boj1463_1로만들기 {
+
+	public static int N;
+	public static int cnt;
+	public static int[] dp = new int[N + 1];
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+	}
+
+	public static int toOne(int N) {
+		if (N == 1) {
+			return 0;
+		}
+		if (N == 2) {
+			return 1;
+		}
+		if (N == 3) {
+			return 1;
+		} else {
+			if (N % 3 == 0) {
+
+			}
+		}
+
+	}
+
+}
