def4a84b1f8c7b80608a85407dba708210c379cc
JiHyeon Jeong <gus4805@naver.com>
2022-05-19 01:30:34 +0900

    Updated On May 19 01:30

---
+++ boj/src/boj/bronze5/Boj1000_AplusB.java
@@ -0,0 +1,14 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Boj1000_AplusB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int ans = A + B;
+		
+		System.out.println(ans);
+	}
+}
