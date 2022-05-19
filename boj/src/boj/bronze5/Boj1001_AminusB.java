8297ace603259e8f4250ec48d7c4a24c6522301d
JiHyeon Jeong <gus4805@naver.com>
2022-05-20 00:28:16 +0900

    Updated On May 20 00:28

---
+++ boj/src/boj/bronze5/Boj1001_AminusB.java
@@ -0,0 +1,17 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Boj1001_AminusB {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int ans = A - B;
+		
+		System.out.println(ans);
+	}
+
+}
