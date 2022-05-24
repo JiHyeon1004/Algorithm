54c9c3053b78b8b0aa774c9bfacba80bc59c3bbd
JiHyeon Jeong <gus4805@naver.com>
2022-05-25 02:12:24 +0900

    Updated On May 25 02:12

---
+++ boj/src/boj/bronze5/Boj1008_AdivisionB.java
@@ -0,0 +1,16 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Boj1008_AdivisionB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		double ans = A/B;
+		
+		System.out.println(ans);
+	}
+
+}
