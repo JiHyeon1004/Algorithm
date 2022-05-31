57bca8f5805e67656c084a2c58dbd1fcfa59476d
JiHyeon Jeong <gus4805@naver.com>
2022-05-31 18:42:11 +0900

    Updated On May 31 18:42

---
+++ boj/src/boj/bronze2/Boj1152_단어의개수.java
@@ -0,0 +1,18 @@
+package bronze2;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Boj1152_단어의개수 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(str);
+		
+		System.out.println(st.countTokens());
+		
+	}
+}
