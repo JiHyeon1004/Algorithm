1392f8315254bb9c1c36b0ea844eca2a1c4a092a
JiHyeon Jeong <gus4805@naver.com>
2022-06-03 00:01:45 +0900

    Updated On June 03 00:01

---
+++ boj/src/boj/bronze3/Boj2438_별찍기1.java
@@ -0,0 +1,18 @@
+package bronze3;
+
+import java.util.Scanner;
+
+public class Boj2438_별찍기1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <= N; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
