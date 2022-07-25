1d8027eccdc5a0aee0debe32507765666621cdee
JiHyeon Jeong <gus4805@naver.com>
2022-07-25 20:03:14 +0900

    [Algorithm]Boj10998_AmultipleB

---
+++ boj/src/boj/bronze5/Boj10998_AmultipleB.java
@@ -0,0 +1,13 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Boj10998_AmultipleB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		System.out.println(A * B);
+	}
+}
