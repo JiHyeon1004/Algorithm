ca0654e76a0e75a625c0e4e9a63404a22d62c445
JiHyeon Jeong <gus4805@naver.com>
2022-08-09 09:54:12 +0900

    [Algorithm]Boj10171_고양이

---
+++ boj/src/boj/bronze5/Boj10171_고양이.java
@@ -0,0 +1,9 @@
+package bronze5;
+
+public class Boj10171_고양이 {
+	
+	//자바에서 \는 단독으로 출력할 수 없으므로 \\ 두 번 써 줘야한다.
+	public static void main(String[] args) {
+		System.out.print("\\    /\\\n" + " )  ( ')\n" + "(  /  )\n" + " \\(__)|");
+	}
+}
