bcc6192b834dc07528dd2d305cee7cb98c00f33a
JiHyeon Jeong <gus4805@naver.com>
2022-08-16 20:52:18 +0900

    [Algorithm]Boj10952_AplusB5

---
+++ boj/src/boj/bronze5/Boj10952_AplusB5.java
@@ -0,0 +1,18 @@
+package bronze5;
+
+import java.util.Scanner;
+
+public class Boj10952_AplusB5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			if (A == 0 && B == 0) {
+				break;
+			}
+			System.out.println(A + B);
+		}
+	}
+}
